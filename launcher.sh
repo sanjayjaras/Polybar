e9601241b17ba4e5348310a1e2626f8d3e486fbc
Sanjay <sanjayjaras@gmail.com>
2021-03-10 22:01:39 -0600

    Added transparency

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-polybar -c ~/.config/polybar/polybar.config mybar 2>&1 & disown
-polybar -c ~/.config/polybar/polybar.config mybar2 2>&1 & disown
+polybar -r -c ~/.config/polybar/polybar.config mybar 2>&1 & disown
+polybar -r -c ~/.config/polybar/polybar.config mybar2 2>&1 & disown
 #polybar -c ~/.config/polybar/polybar2.config mybar2 2>&1 & disown
 
