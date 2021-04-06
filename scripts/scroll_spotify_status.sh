e85654dd170bc5f71c80a3bee1bc0210ffc092ae
sanjayjaras <sanjayjaras@gmail.com>
2021-04-06 17:14:20 -0500

    some tweaks

--- scripts/scroll_spotify_status.sh
+++ scripts/scroll_spotify_status.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # see man zscroll for documentation of the following parameters
-/home/sanjay/anaconda3/bin/zscroll -l 30 \
-        --delay 0.1 \
-        --scroll-padding "  " \
-        --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
-        --match-text "Playing" "--scroll 1" \
-        --match-text "Paused" "--scroll 0" \
-        --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" &
+# /home/sanjay/anaconda3/bin/zscroll -l 30 \
+#         --delay 0.4 \
+#         --scroll-padding "  " \
+#         --match-command "$HOME/.config/polybar/scripts/get_spotify_status.sh --status" \
+#         --match-text "Playing" "--scroll 1" \
+#         --match-text "Paused" "--scroll 0" \
+#         --update-check true "$HOME/.config/polybar/scripts/get_spotify_status.sh" &
 
-# mem="$($HOME/.config/polybar/scripts/get_spotify_status.sh --status )"
-# song="$($HOME/.config/polybar/scripts/get_spotify_status.sh)"
-# echo " "$mem" "$song
-# test=$(/home/sanjay/anaconda3/bin/zscroll)
-wait
+
+# wait
+prefix=""
+status=$($HOME/.config/polybar/scripts/get_spotify_status.sh --status)
+if [[ $status == "Playing" || $status == "Paused" ]]; then
+prefix=$status
+fi
+msg=$($HOME/.config/polybar/scripts/get_spotify_status.sh)
+echo $prefix $msg|cut -c 1-50
