570b2b4daf48daccf893a34f0fd28b8449cc338a
sanjayjaras <sanjayjaras@gmail.com>
2021-05-16 15:51:05 -0500

    changed config

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#killall polybar
+sleep 2
+
 polybar -r -c ~/.config/polybar/polybar.config mybar 2>&1 & disown
 polybar -r -c ~/.config/polybar/polybar.config mybar2 2>&1 & disown
 #polybar -c ~/.config/polybar/polybar2.config mybar2 2>&1 & disown
